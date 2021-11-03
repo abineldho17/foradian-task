e7d7bebaea8f115d947eb2fe3c08b0fdd9070577
abineldho17 <abin.eldho@foradian.com>
2021-11-03 15:26:57 +0530

    second commit

--- style.js
+++ style.js
@@ -2,18 +2,18 @@ let fetchBtn = document.getElementById('fetchBtn');
 fetchBtn.addEventListener('click', showDiv);
 function showDiv() {
     document.getElementById('hidden').style.display = "block";
-    document.getElementById('editablediv').style.display = "none";
- }
- function disappear() {
-    document.getElementById('hidden').style.display = "none";
-    document.getElementById('editablediv').style.display = "block";
- }
- 
+    // document.getElementById('editablediv').style.display = "none";
+}
+//function disappear() {
+//  document.getElementById('hidden').style.display = "none";
+// document.getElementById('editablediv').style.display = "block";
+//}
+
 function getData() {
 
     const xhr = new XMLHttpRequest();
 
-    xhr.open('GET', 'https://reqres.in/api/users?page=2', true);
+    xhr.open('GET', 'https://6180b83b8bfae60017adfbb5.mockapi.io/api/users', true);
 
 
     xhr.onload = function () {
@@ -22,50 +22,117 @@ function getData() {
             let list = document.getElementById('list');
 
             for (let i = 0; i < obj.data.length; i++) {
+
                 var li = document.createElement('li');
                 li.appendChild(document.createTextNode(obj.data[i].first_name + "" + obj.data[i].last_name));
                 list.appendChild(li);
 
 
                 li.addEventListener('click', function () {
-                    console.log(obj.data[i].first_name + " " + obj.data[i].last_name);
-                    let text = obj.data[i].first_name + " " + obj.data[i].last_name;
-                    let text2 = "Email: " + "  " + obj.data[i].email;
-                    document.getElementById("demo2").innerHTML = text2;
-                    document.getElementById("demo").innerHTML = text;
-                    console.log(obj.data[i]);
-                    // document.getElementById("image-holder").innerHTML = img.src = obj.data[i].avatar;
-                    // document.getElementById("image-holder").innerHTML ="<img src = obj.data[i].avatar/>" ;
 
-                   // var img = document.createElement('img');
-                   // img.src = obj.data[i].avatar;
-                
-                   // console.log(obj.data[i].avatar);
-                    //document.getElementById("image-holder").appendChild(img);
+                    let name = obj.data[i].first_name + " " + obj.data[i].last_name;
+                    let email = "Email: " + "  " + obj.data[i].email;
+                    document.getElementById("emailaddress").innerHTML = email;
+                    document.getElementById("username").innerHTML = name;
+
+                    let user = obj.data[i].id;
                     let img = document.getElementById('imagex');
-                    img.src=obj.data[i].avatar;
+                    img.src = obj.data[i].avatar;
 
 
-                   //// let text3 = "Edit";
-                   // document.getElementById("demo3").innerHTML = text3;
+                    document.getElementById('editbtn').addEventListener('click', function () {
 
-                   // document.getElementsByClassName("hidden").style.display = "none";
-                   
-                   let text4 = obj.data[i].first_name + " " + obj.data[i].last_name;
-                   let text5 = "Email: " + " " + obj.data[i].email;
-                   document.getElementById("demo4").innerHTML = text4;
-                   document.getElementById("demo5").innerHTML = text5;
 
-                   let image = document.getElementById('imagey');
-                   image.src=obj.data[i].avatar;
+                        let textfield = document.createElement("input");
+                        textfield.type = "text";
 
-                  // var btn = document.getElementById("demo3");
-                  // if(btn.innerText=="edit"){
-                   //////   btn.innerText="hide";
-                   ///  }
-                  // else{
-                   //  btn.innerText="edit";
-                    // }
+                        textfield.value = obj.data[i].first_name;
+                        document.getElementById('fname').appendChild(textfield);
+
+
+                        let textfield2 = document.createElement("input");
+                        textfield2.type = "text";
+
+                        textfield2.value = obj.data[i].email;
+                        document.getElementById('useremail').appendChild(textfield2);
+
+                        let textfield3 = document.createElement("input");
+                        textfield3.type = "text";
+
+                        textfield3.value = obj.data[i].last_name;
+                        document.getElementById('lname').appendChild(textfield3);
+
+                        document.getElementById("editbtn").style.display = "none";
+                        document.getElementById("username").style.display = "none";
+                        document.getElementById("emailaddress").style.display = "none";
+
+                        document.getElementById("savebtn").style.display = "block";
+                        document.getElementById("fname").style.display = "block";
+                        document.getElementById("useremail").style.display = "block";
+                        document.getElementById("lname").style.display = "block";
+
+
+
+
+
+
+                        document.getElementById('savebtn').addEventListener('click', function () {
+
+                            // let myjs = JSON.stringify(obj.data[i]);
+                            // myjs = myjs.replace(textfield, textfield.value);
+                            // myjs = myjs.replace(textfield3, textfield3.value);
+
+                            // myjs = myjs.replace(textfield2, textfield2.value);
+                            // console.log(textfield, textfield.value);
+                            // console.log(textfield3, textfield3.value);
+                            //console.log(textfield2, textfield2.value);
+                            //newjs = JSON.stringify(myjs);
+
+                            let fullData = { "email": textfield2.value, "first_name": textfield.value, "last_name": textfield3.value };
+                            let fullDataarray = JSON.stringify(fullData);
+                            console.log(obj.data[i]);
+                            // console.log(newjs);
+
+                            // Content-type:  application/JSON; 
+
+                            // Header("Content-type:  application/JSON"); 
+
+
+                            const update_req = new XMLHttpRequest();
+                           // update_req.setRequestHeader("Content-Type", "application/json")
+
+                            // Header("Content-type:  application/JSON"); 
+                            // AjaxReq.setRequestHeader("Content-Type", "application/JSON");
+
+                            // let url = "https://6180b83b8bfae60017adfbb5.mockapi.io/api/users/"+user;
+                            // console.log(url);
+                            update_req.open('PUT', "https://6180b83b8bfae60017adfbb5.mockapi.io/api/users/" + user);
+
+                            update_req.setRequestHeader("Content-Type", "application/json");
+                            update_req.send(fullDataarray);
+                            update_req.onload = function () {
+                                if (this.status === 200) {
+                                
+                                     location.reload();
+                                }
+                            }
+
+                        })
+
+                    })
+
+
+
+
+
+                    // let text4 = obj.data[i].first_name + " " + obj.data[i].last_name;
+                    //let text5 = "Email: " + " " + obj.data[i].email;
+                    //document.getElementById("demo4").innerHTML = text4;
+                    // document.getElementById("demo5").innerHTML = text5;
+
+                    // let image = document.getElementById('imagey');
+                    //image.src=obj.data[i].avatar;
+
                 })
             }
 
@@ -80,4 +147,3 @@ function getData() {
     xhr.send();
 }
 getData()
-
